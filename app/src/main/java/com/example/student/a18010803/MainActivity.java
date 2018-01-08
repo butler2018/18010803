5476d8137d092c3d027860664c22f32ce773edac
calvin <butler2018@hotmail.com>
2018-01-08 13:51:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/a18010803/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.student.a18010803;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lv;
+    String str[] = {"AA", "BB", "CCC", "DD", "FF"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lv = (ListView) findViewById(R.id.listView);
+        MyAdapter adapter = new MyAdapter();
+        lv.setAdapter(adapter);
+    }
+
+    class MyAdapter extends BaseAdapter
+    {
+        @Override
+        public int getCount() {
+            return str.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup viewGroup) {
+            TextView tv = new TextView(MainActivity.this);
+            tv.setText(str[position]);
+            return tv;
+        }
+    }
+}
